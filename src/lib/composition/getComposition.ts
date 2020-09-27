98114612ec58572290f4b3953400424d77114de6
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-09-27 15:34:47 +0200

    Simplify getCompositions

--- src/lib/composition/getComposition.ts
+++ src/lib/composition/getComposition.ts
@@ -1,13 +1,10 @@
 import { parseFormula } from "../parser";
-import { Formula } from "../Formula";
 import { visitFormula } from "../Visitor";
 import { Composition } from "./Composition";
 
 export function getComposition(input: string): Composition {
   const formula = parseFormula(input);
-  return getFormulaComposition(formula);
-}
-function getFormulaComposition(formula: Formula) {
+
   return visitFormula<Composition>(formula, {
     formula(formula, visit) {
       return formula.reduce((composition, block) => {
